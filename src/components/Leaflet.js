40987c2bbcfcef554e14df1363a72092d5ca33d3
heavenwebdev <georgecupid1126@gmail.com>
2022-11-12 20:53:25 -0500

    openstreetmap

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -1,13 +1,21 @@
 
-import React from "react";
+import React, { useState, Fragment } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faAngleUp, faArrowDown, faArrowUp, faEdit, faEllipsisH, faExternalLinkAlt, faEye, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { Col, Row, Nav, Card, Image, Button, Table, Dropdown, ProgressBar, Pagination, ButtonGroup } from '@themesberg/react-bootstrap';
-
+import { Col, Row, Nav, Card, Button, Table, Dropdown, ProgressBar, Pagination, ButtonGroup } from '@themesberg/react-bootstrap';
 import { pageVisits } from "../data/tables";
 
 import { MapContainer, TileLayer } from "react-leaflet";
 import RoutineMachine from "./RountineMachine";
+var mqtt = require("mqtt");
+var options = {
+  protocol: "mqtts",
+  clientId: 'scooterClient',
+  username: 'serverclient',
+  password: 'password'
+};
+var client = mqtt.connect('mqtt://101.37.148.19:1883', options);
+client.subscribe('bike');
 
 export const PageVisitsTable = () => {
   const TableRow = (props) => {
@@ -58,19 +66,28 @@ export const PageVisitsTable = () => {
 };
 
 export const ScootersInUse = () => {
+  var note;
+  client.on('message', function (topic, message) {
+    note = message.toString();
+    setMesg(note);
+    console.log(note);
+    client.end();
+  });
+
+  const [mesg, setMesg] = useState(<Fragment><em>nothing heard</em></Fragment>);
   return (
     <Card border="light" className="shadow-sm p-4">
       <MapContainer
         doubleClickZoom={false}
-        id="mapId"
+        id="scooterinusemap"
         zoom={14}
-        center={[33.5024, 36.2988]}
+        center={[43.6532, 79.3832]}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <RoutineMachine />
+        {/* <RoutineMachine /> */}
       </MapContainer>
     </Card>
   );
@@ -81,15 +98,15 @@ export const AvailableScooters = () => {
     <Card border="light" className="shadow-sm p-4">
       <MapContainer
         doubleClickZoom={false}
-        id="mapId"
+        id="availablescootersmap"
         zoom={14}
-        center={[33.5024, 36.2988]}
+        center={[13.6532, 59.3832]}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <RoutineMachine />
+        {/* <RoutineMachine /> */}
       </MapContainer>
     </Card>
   );
@@ -100,15 +117,15 @@ export const LowBatteryScooters = () => {
     <Card border="light" className="shadow-sm p-4">
       <MapContainer
         doubleClickZoom={false}
-        id="mapId"
+        id="lowbatteryscootersmap"
         zoom={14}
-        center={[33.5024, 36.2988]}
+        center={[73.6532, 79.3832]}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <RoutineMachine />
+        {/* <RoutineMachine /> */}
       </MapContainer>
     </Card>
   );
