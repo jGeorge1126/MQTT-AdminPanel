582d6db4eba63b89c64192ad0f96cbb5d8a263cb
heavenwebdev <georgecupid1126@gmail.com>
2022-11-13 20:00:51 -0500

    fix

---
+++ build/precache-manifest.794bccbf5220c0767f3355e5d0b2b71f.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88558fed431e7cec0f7620f6193924e3",
+    "url": "/volt-react-dashboard/index.html"
+  },
+  {
+    "revision": "2581c27eef8f154b6a9d",
+    "url": "/volt-react-dashboard/static/css/2.e94d5e4c.chunk.css"
+  },
+  {
+    "revision": "ffbbc1bb99c9a3a476e5",
+    "url": "/volt-react-dashboard/static/css/main.936319b3.chunk.css"
+  },
+  {
+    "revision": "2581c27eef8f154b6a9d",
+    "url": "/volt-react-dashboard/static/js/2.62f290f6.chunk.js"
+  },
+  {
+    "revision": "c0b335c8af348b9f9408dc87f31aea85",
+    "url": "/volt-react-dashboard/static/js/2.62f290f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbbc1bb99c9a3a476e5",
+    "url": "/volt-react-dashboard/static/js/main.03be71ae.chunk.js"
+  },
+  {
+    "revision": "321db94fc0ca3ce27330",
+    "url": "/volt-react-dashboard/static/js/runtime-main.fefcaadb.js"
+  },
+  {
+    "revision": "9ba4b7cbe7f072ea0fe50ec0d4b6c97b",
+    "url": "/volt-react-dashboard/static/media/404.9ba4b7cb.jpg"
+  },
+  {
+    "revision": "a925745ba251f066665a252601075e16",
+    "url": "/volt-react-dashboard/static/media/404.a925745b.svg"
+  },
+  {
+    "revision": "192d8f70e8dbebb4b03f98db1a6434c7",
+    "url": "/volt-react-dashboard/static/media/500.192d8f70.jpg"
+  },
+  {
+    "revision": "a621f33521a64bcb6649d36d99941d5f",
+    "url": "/volt-react-dashboard/static/media/500.a621f335.svg"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/volt-react-dashboard/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "95abb40d53edf7f917949e8757cdf5c6",
+    "url": "/volt-react-dashboard/static/media/bs5-illustrations.95abb40d.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/volt-react-dashboard/static/media/canada.ed3cd4b5.svg"
+  },
+  {
+    "revision": "935e70467bab0575ad62110dfae45476",
+    "url": "/volt-react-dashboard/static/media/forgot-password.935e7046.jpg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/volt-react-dashboard/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/volt-react-dashboard/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/volt-react-dashboard/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/volt-react-dashboard/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "9f0623ff820f2fde19c6c5dcb3308c39",
+    "url": "/volt-react-dashboard/static/media/laravel-logo.9f0623ff.svg"
+  },
+  {
+    "revision": "69f2db42fdfeb91a54fe1573b6b102c9",
+    "url": "/volt-react-dashboard/static/media/lock.69f2db42.jpg"
+  },
+  {
+    "revision": "b2d9831a6290a67a05fa6d65bb67d226",
+    "url": "/volt-react-dashboard/static/media/mockup-calendar-presentation.b2d9831a.png"
+  },
+  {
+    "revision": "fc6d4c754fd2b26a2e4775bc91b61bbc",
+    "url": "/volt-react-dashboard/static/media/mockup-map-presentation.fc6d4c75.png"
+  },
+  {
+    "revision": "77a4c6a89e893405dd125243971c3d61",
+    "url": "/volt-react-dashboard/static/media/mockup-presentation.77a4c6a8.png"
+  },
+  {
+    "revision": "00a526136ebe6ca9adbd18a0dcb9064d",
+    "url": "/volt-react-dashboard/static/media/overview.00a52613.jpg"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/volt-react-dashboard/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/volt-react-dashboard/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/volt-react-dashboard/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/volt-react-dashboard/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/volt-react-dashboard/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/volt-react-dashboard/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/volt-react-dashboard/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "5e51d949230c7a92d64c73604e5a3243",
+    "url": "/volt-react-dashboard/static/media/react-logo-primary.5e51d949.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/volt-react-dashboard/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "9675648e16658f86badd668a84c7f3b7",
+    "url": "/volt-react-dashboard/static/media/react-logo.9675648e.svg"
+  },
+  {
+    "revision": "99d034b6cea27baae946ce783434954f",
+    "url": "/volt-react-dashboard/static/media/react-mockup.99d034b6.png"
+  },
+  {
+    "revision": "0a4cc9cd55c1c298542e559d34622a48",
+    "url": "/volt-react-dashboard/static/media/reset-password.0a4cc9cd.jpg"
+  },
+  {
+    "revision": "43fe41840c256623201cc14f63415614",
+    "url": "/volt-react-dashboard/static/media/settings.43fe4184.jpg"
+  },
+  {
+    "revision": "ea051702c568e9d0199b6ddc1a52fff4",
+    "url": "/volt-react-dashboard/static/media/sign-in.ea051702.jpg"
+  },
+  {
+    "revision": "6774df144d28e4f7c55c1f7725823465",
+    "url": "/volt-react-dashboard/static/media/sign-up.6774df14.jpg"
+  },
+  {
+    "revision": "1217b8ee169b233ae80821ff35d74509",
+    "url": "/volt-react-dashboard/static/media/signin.1217b8ee.svg"
+  },
+  {
+    "revision": "bbd344a3e9c39621e69a64c02e4b11de",
+    "url": "/volt-react-dashboard/static/media/themesberg-logo.bbd344a3.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/volt-react-dashboard/static/media/themesberg.105e5059.svg"
+  },
+  {
+    "revision": "b98aee40114ed3eaae525ce4ec63b0c3",
+    "url": "/volt-react-dashboard/static/media/transactions.b98aee40.jpg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/volt-react-dashboard/static/media/united-kingdom.e5564902.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/volt-react-dashboard/static/media/united-states-of-america.36ab476e.svg"
+  }
+]);
--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -1,21 +1,18 @@
 
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faAngleUp, faArrowDown, faArrowUp, faEdit, faEllipsisH, faExternalLinkAlt, faEye, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { Col, Row, Nav, Card, Button, Table, Dropdown, ProgressBar, Pagination, ButtonGroup } from '@themesberg/react-bootstrap';
 import { pageVisits } from "../data/tables";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import socketIOClient from "socket.io-client";
 import RoutineMachine from "./RountineMachine";
-var mqtt = require("mqtt");
-var options = {
-  protocol: "mqtts",
-  clientId: 'scooterClient',
-  username: 'serverclient',
-  password: 'password'
-};
-var client = mqtt.connect('mqtt://101.37.148.19:1883', options);
-client.subscribe('bike');
+import icon from "../marker-icon.png";
+
+// const ENDPOINT = "http://127.0.0.1:8000";
+const ENDPOINT = "http://54.89.211.240:8000";
+const socket = socketIOClient(ENDPOINT);
 
 export const PageVisitsTable = () => {
   const TableRow = (props) => {
@@ -66,17 +63,9 @@ export const PageVisitsTable = () => {
 };
 
 export const ScootersInUse = () => {
-  var note;
-  client.on('message', function (topic, message) {
-    note = message.toString();
-    setMesg(note);
-    console.log(note);
-    client.end();
-  });
-
-  const [mesg, setMesg] = useState(<Fragment><em>nothing heard</em></Fragment>);
   return (
     <Card border="light" className="shadow-sm p-4">
+      <h5></h5>
       <MapContainer
         doubleClickZoom={false}
         id="scooterinusemap"
@@ -94,18 +83,76 @@ export const ScootersInUse = () => {
 };
 
 export const AvailableScooters = () => {
+  const [scootermarkers, setScootermarker] = useState([]);
+  
+  useEffect(() => {
+    socket.on("sendMessage", data => {
+      console.log(data);
+      var jsonResult = JSON.parse(data.text);
+      
+      let updatedvalue = {
+        i: jsonResult.i,
+      };
+      const prevScooterIndex = scootermarkers.findIndex(s => s.i === jsonResult.i)
+      if (prevScooterIndex !== -1) {
+        updatedvalue = {
+          ...scootermarkers[prevScooterIndex],
+        }
+      }
+
+      if(jsonResult.a == 19){
+        var temp = jsonResult.g.split(",");
+        
+        updatedvalue.lat = temp[0]
+        updatedvalue.lng = temp[1]
+      } else if(jsonResult.a == 27){
+        updatedvalue.b = jsonResult.b
+      }
+      
+      setScootermarker(p => {
+        let prevScooters = [...p]
+        const prevScooterIndex = p.findIndex(s => s.i === updatedvalue.i)
+        if (prevScooterIndex === -1) {
+          prevScooters.push(updatedvalue)
+        } else {
+          prevScooters[prevScooterIndex] = {
+            ...prevScooters[prevScooterIndex],
+            ...updatedvalue
+          }
+        }
+        console.log(prevScooters, "prevScooters")
+        return [...prevScooters]
+      })
+      // if(jsonResult["i"] == "TEST-999666" && jsonResult["a"] == 19){
+      //   var temp = jsonResult["g"].split(",");
+      //   setResponselat(temp[0]);
+      //   setResponselng(temp[1]);
+      // }
+      // if(jsonResult["i"] == "TEST-999666" && jsonResult["a"] == 27){
+      //   setResponsebat(jsonResult["b"]);
+      // }
+    });
+  }, []);
+  console.log(scootermarkers);
   return (
     <Card border="light" className="shadow-sm p-4">
       <MapContainer
         doubleClickZoom={false}
         id="availablescootersmap"
         zoom={14}
-        center={[13.6532, 59.3832]}
+        center={[47.90722,13.76072]}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        { (scootermarkers || []).filter(s => s.lat && s.lng).map(s => 
+          <Marker position={[s.lat, s.lng]}>
+            <Popup>
+              { s.battery >= 0 ? s.b : '' }%
+            </Popup>
+          </Marker>
+        )}
         {/* <RoutineMachine /> */}
       </MapContainer>
     </Card>
